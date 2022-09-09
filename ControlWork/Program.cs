0e1eec3b19eb2de3c3a7d3bb876bea9d31b1888f
Elizaveta Plokhotnikova <lizapd@icloud.com>
2022-09-09 15:38:19 +0700

    Added ControlWork

---
+++ ControlWork/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Пожалуйста, введите нужные слова через пробел:");
+// Используем функцию в классе String, которая может разделить строку
+// на слова через указанный символ: функция Split.
+String[] words = Console.ReadLine().Split(' '); // Разделили строку на массив слов
+for(int i = 0; i < words.Length; i++) // Проходим по каждому слову
+{
+    if(words[i].Length <= 3) // Проверяем длинну слова
+    {
+        Console.WriteLine(words[i]);
+    }
+}
+Console.WriteLine();
